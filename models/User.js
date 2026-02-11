a990a35a194a5b9b29bce5e865ff518d44264c2c
101501186 <101501186@georgebrown.ca>
2026-02-11 08:06:31 -0600

    Created User schema

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  firstname: String,
+  lastname: String,
+  password: String,
+  createdon: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", userSchema);
